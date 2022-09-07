3c594a5e6846351bca04d940181e0f87df9229ee
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-09-07 07:23:38 +0200

    Introduced the separate delegate class

---
+++ EmojiLibrary/EmojiCollectionViewDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  EmojiCollectionViewDelegate.swift
+//  EmojiLibrary
+//
+//  Created by Pat on 2022/09/07.
+//
+
+
+import UIKit
+
+class EmojiCollectionViewDelegate: NSObject, UICollectionViewDelegateFlowLayout{
+    let numberOfItemsPerRow: CGFloat
+    let interItemSpacing: CGFloat
+    
+    init(numberOfItemsPerRow: CGFloat, interItemSpacing:CGFloat){
+        self.numberOfItemsPerRow = numberOfItemsPerRow
+        self.interItemSpacing = interItemSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxWidth = UIScreen.main.bounds.width
+        let totalSpacing = interItemSpacing * numberOfItemsPerRow
+        
+        let itemWidth = (maxWidth - totalSpacing)/numberOfItemsPerRow
+        return CGSize(width: itemWidth, height: itemWidth)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return interItemSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        //The first section gets a normal layout and the rest get spaced accordinly
+        if section == 0{
+            return UIEdgeInsets(top: 0, left: 0, bottom: interItemSpacing/2, right: 0)
+        }else{
+            return UIEdgeInsets(top: interItemSpacing/2, left: 0, bottom: interItemSpacing/2, right: 0)
+        }
+    }
+    
+}
--- EmojiLibrary/ViewController.swift
+++ EmojiLibrary/ViewController.swift
@@ -5,11 +5,13 @@ class ViewController: UIViewController {
 
   @IBOutlet weak var collectionView: UICollectionView!
     let dataSource = DataSource()
+    let delegate = EmojiCollectionViewDelegate(numberOfItemsPerRow: 6, interItemSpacing: 8)
     
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
       collectionView.dataSource = dataSource
+      collectionView.delegate = delegate
   }
 
 }
